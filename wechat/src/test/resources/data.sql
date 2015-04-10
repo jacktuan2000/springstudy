fab042e8734e5a986b59fadf64643a4da37d6118
gejj <gejj@yonyou.com>
2015-04-10 13:35:37 +0800

    持续集成测试
    Change-Id: I76c7e00d0c615b2d2b631e5956f8a56d6883862e
    Signed-off-by: gejj <gejj@yonyou.com>

--- wechat/src/test/resources/data.sql
+++ wechat/src/test/resources/data.sql
@@ -1,11 +1,11 @@
 
-INSERT INTO WE_User (id, modifiedDate, accountNonExpired, accountNonLocked, credentialsNonExpired, email, enabled, firstLogin, mobile, nickName, password, username) VALUES (3, null, true, true, true, null, true, true, null, null, 'admin', 'admin');
-INSERT INTO WE_User (id, modifiedDate, accountNonExpired, accountNonLocked, credentialsNonExpired, email, enabled, firstLogin, mobile, nickName, password, username) VALUES (4, null, true, true, true, null, true, true, null, null, 'user', 'user');
+--INSERT INTO WE_User (id, modifiedDate, accountNonExpired, accountNonLocked, credentialsNonExpired, email, enabled, firstLogin, mobile, nickName, password, username) VALUES (3, null, true, true, true, null, true, true, null, null, 'admin', 'admin');
+--INSERT INTO WE_User (id, modifiedDate, accountNonExpired, accountNonLocked, credentialsNonExpired, email, enabled, firstLogin, mobile, nickName, password, username) VALUES (4, null, true, true, true, null, true, true, null, null, 'user', 'user');
 
-INSERT INTO WE_Resource (id, modifiedDate, Name, content, enabled, remark, type) VALUES (1, null, '所有页面', '/**', true, '所有页面', 'requesturl');
-INSERT INTO WE_Resource (id, modifiedDate, Name, content, enabled, remark, type) VALUES (2, null, '人员', '/person/*', true, '人员', 'requesturl');
+--INSERT INTO WE_Resource (id, modifiedDate, Name, content, enabled, remark, type) VALUES (1, null, '所有页面', '/**', true, '所有页面', 'requesturl');
+--INSERT INTO WE_Resource (id, modifiedDate, Name, content, enabled, remark, type) VALUES (2, null, '人员', '/person/*', true, '人员', 'requesturl');
 
 
-INSERT INTO WE_Authoritie (id, modifiedDate, authority, user_id) VALUES (1, null, 'ROLE_ADMIN', 3);
-INSERT INTO WE_Authoritie (id, modifiedDate, authority, user_id) VALUES (2, null, 'ROLE_USER', 3);
-INSERT INTO WE_Authoritie (id, modifiedDate, authority, user_id) VALUES (3, null, 'ROLE_USER', 4);
+--INSERT INTO WE_Authoritie (id, modifiedDate, authority, user_id) VALUES (1, null, 'ROLE_ADMIN', 3);
+--INSERT INTO WE_Authoritie (id, modifiedDate, authority, user_id) VALUES (2, null, 'ROLE_USER', 3);
+--INSERT INTO WE_Authoritie (id, modifiedDate, authority, user_id) VALUES (3, null, 'ROLE_USER', 4);
