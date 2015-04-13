314fd93ca9f8e3166932fa9537d53527798e688c
gejj <gejj@yonyou.com>
2015-04-13 14:56:20 +0800

    test
    Change-Id: I0a0b82eae485fa93820eeb40ac4bd32ec9d626bc
    Signed-off-by: gejj <gejj@yonyou.com>

--- wechat/src/main/java/com/yonyou/up/app/Application.java
+++ wechat/src/main/java/com/yonyou/up/app/Application.java
@@ -28,9 +28,12 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
        
     	
+    	
+    	
+    	
     	SpringApplication.run(Application.class, args);
         
-    	HAHA
+    
     }
 
 }
